9d11601cd2dd5efcaa7422bf273d989f2db2f52e
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2024-05-18 21:21:42 +0600

    Logging Added

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,13 +1,15 @@
 using CompanyEmployees.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
-
+using NLog;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),"/nlog.config"));
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
